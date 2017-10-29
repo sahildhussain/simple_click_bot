55169352938696b68d8139480e779249b7baeae6
Sahil Hussain <sahil.hussain113@gmail.com>
2017-10-29 19:30:17 +0530

    Update open_site.py

--- open_site.py
+++ open_site.py
@@ -7,11 +7,14 @@ for i in range(1,32,1):
     time.sleep(k+3*k+k/2)
     #i am using 3 conditions, you may use as much conditions as you want.
     if k%3==0:
-        webbrowser.open("http://www.myewizard.com/advertise.php?j_id = "+str(i+k))
+        webbrowser.open("http://www.yourptcsite.com/complete/url.php = "+str(i+k))
         time.sleep(13+k)
+        pyautogui.click(AAAAA+k,BBBB+k)
     elif k%5==0:
-        webbrowser.open("http://www.myewizard.com/jobs.php")
+        webbrowser.open("http://www.yourptcsite2.com/complete/url.php")
+        pyautogui.click(AAAAA+k,BBBB+k)
     else:
-        webbrowser.open("http://www.myewizard.com/advertise.php")
+        webbrowser.open("http://www.yourptcsite3.com/complete/url.php")
         time.sleep(5)
-        pyautogui.click(120+k,400+k)
+        pyautogui.click(AAAAA+k,BBBB+k)
+#Replace AAAAA and BBBB with approx ad position. lot of tools are available on net.
